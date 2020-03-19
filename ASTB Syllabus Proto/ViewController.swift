aca95974cbc2bb43730e623645320a1fcccb6a02
Alex Hastings <alexhasting@gmail.com>
2020-03-19 16:27:02 -0500

    Initial Commit

---
+++ ASTB Syllabus Proto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ASTB Syllabus Proto
+//
+//  Created by Alex Hastings on 3/19/20.
+//  Copyright © 2020 Alex Hastings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
